82fd98a5abd0226fc09aa21659e6d3a02238fbd4
123zarif <zarifzuhayer20@gmail.com>
2026-02-10 01:11:23 +0600

    Add event time input and update event model to include start and end times

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
 use mongodb::{Client, Collection};
-use serde::{Deserialize, Serialize};
-
 mod models;
 
 use crate::models::events::Event;
@@ -17,6 +15,8 @@ async fn main() -> mongodb::error::Result<()> {
 
     let mut name = String::new();
     let mut event_type = String::new();
+    let mut start_time = String::new();
+    let mut end_time = String::new();
 
     println!("Enter event name:");
     std::io::stdin()
@@ -28,9 +28,21 @@ async fn main() -> mongodb::error::Result<()> {
         .read_line(&mut event_type)
         .expect("Why can't you write name man... Its just a string- Thats embracing!");
 
+    println!("Enter start time (HH:MM):");
+    std::io::stdin()
+        .read_line(&mut start_time)
+        .expect("Invalid time");
+
+    println!("Enter end time (HH:MM):");
+    std::io::stdin()
+        .read_line(&mut end_time)
+        .expect("Invalid time");
+
     Event::add_event(
         &collection,
         name.trim().to_string(),
+        start_time.trim().to_string(),
+        end_time.trim().to_string(),
         vec![2, 3],
         event_type.trim().to_string(),
     )
--- src/models/events.rs
+++ src/models/events.rs
@@ -1,3 +1,4 @@
+use chrono::{NaiveTime, Utc};
 use mongodb::{
     Collection,
     bson::{DateTime, oid::ObjectId},
@@ -10,8 +11,8 @@ pub struct Event {
     #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
     pub id: Option<ObjectId>,
     pub name: String,
-    // pub start_time: DateTime,
-    // pub end_time: DateTime,
+    pub start_time: DateTime,
+    pub end_time: DateTime,
     pub days: Vec<u8>,
     pub event_type: String,
 }
@@ -20,21 +21,33 @@ impl Event {
     pub async fn add_event(
         collection: &Collection<Event>,
         name: String,
-        // start_time: DateTime,
-        // end_time: DateTime,
+        start_time: String,
+        end_time: String,
         days: Vec<u8>,
         event_type: String,
     ) -> mongodb::error::Result<InsertOneResult> {
+        let st = NaiveTime::parse_from_str(&start_time, "%H:%M").expect("Error Parsing time!");
+        let et = NaiveTime::parse_from_str(&end_time, "%H:%M").expect("Error parsing Time!");
+
+        let date = Utc::now().date_naive();
+
+        let start_dt = date.and_time(st);
+        let start_ms = start_dt.and_utc().timestamp_millis();
+
+        let end_dt = date.and_time(et);
+        let end_ms = end_dt.and_utc().timestamp_millis();
+
+        let start_bson_time = DateTime::from_millis(start_ms);
+        let end_bson_time = DateTime::from_millis(end_ms);
+
         let new_event = Event {
             id: None,
             name: name.clone(),
-            // start_time,
-            // end_time,
+            start_time: start_bson_time,
+            end_time: end_bson_time,
             days,
             event_type,
         };
-
-        println!("Tried Adding {}", name);
 
         collection.insert_one(new_event).await
     }
