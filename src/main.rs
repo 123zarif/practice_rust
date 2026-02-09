5c5c96ed3291c830056535cebb1898d0ad629969
123zarif <zarifzuhayer20@gmail.com>
2026-02-09 20:01:01 +0600

    Implement event model and add event functionality with user input

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,40 @@
-use futures::TryStreamExt;
-use mongodb::{Client, Collection, bson::doc};
+use mongodb::{Client, Collection};
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Event {
-    name: String,
-}
+mod models;
+
+use crate::models::events::Event;
 
 #[tokio::main]
 async fn main() -> mongodb::error::Result<()> {
-    let uri = "mongodb+srv://server:server123%40@cluster0.nuhlzny.mongodb.net/?appName=Cluster0";
+    let uri: &str =
+        "mongodb+srv://server:server123%40@cluster0.nuhlzny.mongodb.net/?appName=Cluster0";
 
     let client = Client::with_uri_str(uri).await?;
 
     let db = client.database("Calendar");
-    let cols: Collection<Event> = db.collection("Events");
+    let collection: Collection<Event> = db.collection("Events");
 
-    let mut events = cols.find(doc! {}).await?;
+    let mut name = String::new();
+    let mut event_type = String::new();
 
-    while let Some(event) = events.try_next().await? {
-        println!("{:#?}", event)
-    }
+    println!("Enter event name:");
+    std::io::stdin()
+        .read_line(&mut name)
+        .expect("Why can't you write name man... Its just a string- Thats embracing!");
+
+    println!("Enter event type:");
+    std::io::stdin()
+        .read_line(&mut event_type)
+        .expect("Why can't you write name man... Its just a string- Thats embracing!");
+
+    Event::add_event(
+        &collection,
+        name.trim().to_string(),
+        vec![2, 3],
+        event_type.trim().to_string(),
+    )
+    .await?;
 
     Ok(())
 }
--- src/models/events.rs
+++ src/models/events.rs
@@ -1,11 +1,41 @@
 use mongodb::{
-    oid::ObjectId
-}
+    Collection,
+    bson::{DateTime, oid::ObjectId},
+    results::InsertOneResult,
+};
 use serde::{Deserialize, Serialize};
 
-
-#[derive(Serialize, Deserialize)]
-
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Event {
+    #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
+    pub id: Option<ObjectId>,
+    pub name: String,
+    // pub start_time: DateTime,
+    // pub end_time: DateTime,
+    pub days: Vec<u8>,
+    pub event_type: String,
+}
 
+impl Event {
+    pub async fn add_event(
+        collection: &Collection<Event>,
+        name: String,
+        // start_time: DateTime,
+        // end_time: DateTime,
+        days: Vec<u8>,
+        event_type: String,
+    ) -> mongodb::error::Result<InsertOneResult> {
+        let new_event = Event {
+            id: None,
+            name: name.clone(),
+            // start_time,
+            // end_time,
+            days,
+            event_type,
+        };
+
+        println!("Tried Adding {}", name);
+
+        collection.insert_one(new_event).await
+    }
 }
